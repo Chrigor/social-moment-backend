32e70a6d0590b387ebf574c3737b4b320f2ccf5f
Chrigor <chrigor.edu3@gmail.com>
2020-06-30 10:55:45 -0300

    adicionando modulo de cors, moongose, abstraindo rotas e adicionando variaveis de ambiente

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+require('dotenv').config();
 
 const app = express();
 const port = 3333;
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`O servidor está escutando na porta ${port}`);
